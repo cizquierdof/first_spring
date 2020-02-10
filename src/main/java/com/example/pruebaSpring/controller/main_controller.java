7eb6f02d201b628a0da28f9bcba5ab4cb8a5b9b8
WEB <cizquierdof@gmail.com>
2020-02-10 13:56:31 +0100

    añadida calculadora

--- src/main/java/com/example/pruebaSpring/controller/main_controller.java
+++ src/main/java/com/example/pruebaSpring/controller/main_controller.java
@@ -1,5 +1,6 @@
 package com.example.pruebaSpring.controller;
 
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.ui.Model;
@@ -14,30 +15,63 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class main_controller {
 
-    //@GetMapping("/hola/{name}")
+    // @GetMapping("/hola/{name}")
     @GetMapping("")
     @ResponseBody
-    
-    public String holaMundo(){
-                            
-        //model.addAttribute("name", name);
+
+    public String holaMundo() {
+
+        // model.addAttribute("name", name);
         return "hola mundo";
     }
 
     @GetMapping("/edad")
     @ResponseBody
-    
-    public String holaMundo(@RequestParam("name") String name,
-                            @RequestParam("edad") Integer edad){
-        //model.addAttribute("name", name);
-        return "hola "+name+" tienes "+ edad+" años";
+
+    public String holaMundo(@RequestParam("name") String name, @RequestParam("edad") Integer edad) {
+        // model.addAttribute("name", name);
+        return "hola " + name + " tienes " + edad + " años";
     }
 
     @GetMapping("+")
-    public ModelAndView helloHTM(){
-        ModelAndView respuesta=new ModelAndView("hola");    //objeto hace referencia a la plantilla
-        String msg="Hola mundo HTML!";  //contenido 
-        respuesta.addObject(("name"), msg); //añade contenido string al objeto respuesta
+    public ModelAndView helloHTM() {
+        ModelAndView respuesta = new ModelAndView("hola"); // objeto hace referencia a la plantilla
+        String msg = "Hola mundo HTML!"; // contenido
+        respuesta.addObject(("name"), msg); // añade contenido string al objeto respuesta
         return respuesta;
     }
+
+    @GetMapping("/calc")
+    @ResponseBody
+    public String Calculadora(@RequestParam("op") String op, @RequestParam(name = "op1") Double op1,
+            @RequestParam(name = "op2", required = false) Double op2) {
+        Double resultado = 0.0;
+        String res = "";
+        System.out.println(op + " " + op1 + " " + op2);
+        switch (op) {
+        case "mas":
+            resultado = op1 + op2;
+
+            break;
+        case "-":
+            resultado = op1 - op2;
+            break;
+        case "*":
+            resultado = op1 * op2;
+            break;
+        case "/":
+            resultado = op1 / op2;
+            break;
+        case "sqr":
+            resultado = Math.sqrt(op1);
+            break;
+        default:
+            return "Operación no soportada";
+        }
+
+        res = resultado.toString();
+        return res;
+
+    }
+
 }
