e644711a5b67a84523eea1ee09f757a649bac2a3
WEB <cizquierdof@gmail.com>
2020-02-10 13:56:31 +0100

    añadida calculadora

--- src/main/java/com/example/pruebaSpring/controller/main_controller.java
+++ src/main/java/com/example/pruebaSpring/controller/main_controller.java
@@ -3,8 +3,6 @@ package com.example.pruebaSpring.controller;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
