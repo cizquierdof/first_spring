bc7a3df58596216804147d8de0c7b6b0ad1f487e
WEB <cizquierdof@gmail.com>
2020-02-10 12:24:02 +0100

    usando plantillas

--- src/main/java/com/example/pruebaSpring/controller/main_controller.java
+++ src/main/java/com/example/pruebaSpring/controller/main_controller.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * main_controller
@@ -14,11 +15,29 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class main_controller {
 
     //@GetMapping("/hola/{name}")
-    @GetMapping("/")
+    @GetMapping("")
     @ResponseBody
-    public String holaMundo(@RequestParam("nombre") String name,
+    
+    public String holaMundo(){
+                            
+        //model.addAttribute("name", name);
+        return "hola mundo";
+    }
+
+    @GetMapping("/edad")
+    @ResponseBody
+    
+    public String holaMundo(@RequestParam("name") String name,
                             @RequestParam("edad") Integer edad){
         //model.addAttribute("name", name);
         return "hola "+name+" tienes "+ edad+" años";
+    }
+
+    @GetMapping("+")
+    public ModelAndView helloHTM(){
+        ModelAndView respuesta=new ModelAndView("hola");    //objeto hace referencia a la plantilla
+        String msg="Hola mundo HTML!";  //contenido 
+        respuesta.addObject(("name"), msg); //añade contenido string al objeto respuesta
+        return respuesta;
     }
 }
