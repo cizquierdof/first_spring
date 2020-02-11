84f76b68fd881d68b0d31b4ce912f4ae86f63e0f
WEB <cizquierdof@gmail.com>
2020-02-11 12:56:28 +0100

    formulario y metodo post

--- src/main/java/com/example/pruebaSpring/controller/main_controller.java
+++ src/main/java/com/example/pruebaSpring/controller/main_controller.java
@@ -1,8 +1,8 @@
 package com.example.pruebaSpring.controller;
 
-import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
@@ -31,7 +31,7 @@ public class main_controller {
         return "hola " + name + " tienes " + edad + " años";
     }
 
-    @GetMapping("+")
+    @GetMapping("/hola")
     public ModelAndView helloHTM() {
         ModelAndView respuesta = new ModelAndView("hola"); // objeto hace referencia a la plantilla
         String msg = "Hola mundo HTML!"; // contenido
@@ -39,28 +39,55 @@ public class main_controller {
         return respuesta;
     }
 
-    @GetMapping("/calc")
-    @ResponseBody
-    public String Calculadora(@RequestParam("op") String op, @RequestParam(name = "op1") Double op1,
-            @RequestParam(name = "op2", required = false) Double op2) {
+    @GetMapping("/calculadora")
+    public ModelAndView CalculadoraHTML() {
+        ModelAndView modelAndview =new ModelAndView("hola");
+        modelAndview.addObject("mensaje", "");
+        return modelAndview;
+    }
+
+    @PostMapping("/calculadora")
+    public ModelAndView calculadoraHTMLPost(
+                @RequestParam("op") String op, 
+                @RequestParam(name = "op1") Double op1,
+                @RequestParam(name = "op2", required = false) Double op2){
+                    ModelAndView modelAndView=new ModelAndView("hola");
+
+                    String resultado=calcular(op1,op2,op);
+
+                    modelAndView.addObject("mensaje", "El resultado es: "+resultado);
+
+                    return modelAndView;
+                }
+    
+
+  /*   @GetMapping("/calculadora")
+    public ModelAndView calculadoraHTML() {
+        ModelAndView respuesta = new ModelAndView("hola"); // objeto hace referencia a la plantilla
+        
+        respuesta.addObject(("name"), msg); // añade contenido string al objeto respuesta
+        return respuesta;
+    } */
+
+    private String calcular(Double op1, Double op2, String op) {
         Double resultado = 0.0;
         String res = "";
         System.out.println(op + " " + op1 + " " + op2);
         switch (op) {
-        case "mas":
+        case "suma":
             resultado = op1 + op2;
 
             break;
-        case "-":
+        case "resta":
             resultado = op1 - op2;
             break;
-        case "*":
+        case "multiplica":
             resultado = op1 * op2;
             break;
-        case "/":
+        case "divide":
             resultado = op1 / op2;
             break;
-        case "sqr":
+        case "raiz":
             resultado = Math.sqrt(op1);
             break;
         default:
